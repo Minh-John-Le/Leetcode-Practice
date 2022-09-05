cbd35c91e8b409320cbdfef742c2b2feff1bf70a
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2022-09-05 08:06:46 -0700

    Time: 135 ms (11.82%), Space: 13.1 MB (97.12%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x < 0):
+            return False
+        power = len(str(x)) - 1
+        
+        while(x > 0):
+            
+            largestPlace = x / (10**power)  
+            decimalPlace = x % 10
+
+            if(largestPlace != decimalPlace):
+               return False
+            x  = x - largestPlace * (10**power)
+            x  = x / 10
+            power -= 2;
+            
+        return True    
+            
+            
